6f6dba569d84cb956b9c0e7ea07cb5c759d7826c
Bram Moreinis <bmoreinis@gmail.com>
2021-06-05 22:38:05 +0000

    Lots of moving to template

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ var navBar = document.getElementById("navbarsExampleDefault");
 var folders=["template","captions","chessgo","instahurts","music","pandemic","powerpuff","racism","rockets","romeo","spanish","wagegap"];
 var menu = "<ul class=\"navbar-nav mr-auto\">";
 for (i=0;i<folders.length;i++){
-  menu+="<li class=\"nav-item\"><a class=\"dropdown-item\" href=\""+folders[i]+"\">"+folders[i]+"</a>";
+  menu+="<li class=\"nav-item\"><a class=\"dropdown-item\" href=\""+popper+folders[i]+"\">"+folders[i]+"</a>";
 }
 menu+="</ul>";
 navBar.innerHTML=menu;

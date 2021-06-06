8274b1216213ed56f131cef041a437f6a990061c
Bram Moreinis <bmoreinis@gmail.com>
2021-06-06 21:18:51 +0000

    Ready

--- menu.js
+++ menu.js
@@ -1,8 +1,8 @@
 var navBar = document.getElementById("navbarsExampleDefault");
-var folders=["template","captions","chessgo","instahurts","music","pandemic","powerpuff","racism","rockets","romeo","spanish","wagegap"];
+var folders=[["index.html","Home"],["captions","Captions Contest"],["chessgo","Chess vs. Go"],["instahurts","Social Media Hurts"],["music","Pandemic Music"],["pandemic","Student Mental Health"],["powerpuff","Racist Live Action"],["rockets","Space Race"],["romeo","R&J Memes"],["spanish","Ventajas del Idioma"],["racism","US Racism"],["wagegap","Wage Gap Math"]];
 var menu = "<ul class=\"navbar-nav mr-auto\">";
 for (i=0;i<folders.length;i++){
-  menu+="<li class=\"nav-item\"><a class=\"dropdown-item\" href=\""+popper+folders[i]+"\">"+folders[i]+"</a>";
+  menu+="<li class=\"nav-item\"><a class=\"nav-link\" href=\""+popper+folders[i][0]+"\">"+folders[i][1]+"</a>";
 }
 menu+="</ul>";
 navBar.innerHTML=menu;
